d2239d48868b9d668b9573396245da6a32041cb7
李树钊 <1002749116@qq.com>
2024-03-08 18:24:12 +0800

    build a tag index page

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,12 @@
 export interface Post {
-  frontmatter: {
-    layout: string;
-    title: string;
-    author: string;
-    description: string;
-    image: {
-      url: string;
-      alt: string;
-    };
-    pubDate: Date;
-    tags: string[];
+  layout: string;
+  title: string;
+  author: string;
+  description: string;
+  image: {
+    url: string;
+    alt: string;
   };
-  url: string;
+  pubDate: Date;
+  tags: string[];
 }
